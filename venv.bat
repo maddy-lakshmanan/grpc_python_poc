13560c562b75af40ebe257a111c522c3d58420a5
Lakshmanan Maddy <maddy.lakshmanan@gmail.com>
2025-08-11 21:11:53 -0700

    initialize..

---
+++ venv.bat
@@ -0,0 +1,9 @@
+git config --global usern.name "Maddy"
+git config --global user.email "maddy.lakshmanan@gmail.com"
+
+python -m venv venv
+chmod 755 ./venv/bin/activate
+./venv/bin/activate
+pip3 install -r requirements.txt 
+
+
